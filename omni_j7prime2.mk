fdfea58b4c4286352e85da26e212b8552d83db5f
opuzum <157212352+opuzum@users.noreply.github.com>
2026-03-05 10:49:59 -0800

    Modify device name and identifiers for J7 Prime 2
    Updated device identifiers and product names for J7 Prime 2.

--- omni_j7prime2.mk
+++ omni_j7prime2.mk
@@ -1,19 +1,10 @@
-LOCAL_PATH := device/samsung/j7xelte
-
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
-# Inherit some common Lineage stuff.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_COPY_FILES += \
-     $(LOCAL_PATH)/prebuilt/Image:kernel \
-     $(LOCAL_PATH)/prebuilt/dt.img:dt.img \
-     $(LOCAL_PATH)/prebuilt/dt.img:boot.img
-
-## Device identifier. This must come after all inclusions
-PRODUCT_NAME := omni_j7xelte
-PRODUCT_DEVICE := j7xelte
+PRODUCT_DEVICE := j7prime2
+PRODUCT_NAME := omni_j7prime2
 PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-G611F
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := Samsung Galaxy J7 2016
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
